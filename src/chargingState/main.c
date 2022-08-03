d9bfd7799e52cfe91cfb96ca8d81c47f0d7a9f73
injekim <inje.k@outlook.com>
2022-08-03 04:22:31 -0400

    Added customisation to chargingState

--- src/chargingState/main.c
+++ src/chargingState/main.c
@@ -4,12 +4,47 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <sys/stat.h>
 #include <pthread.h>
 #include <string.h>
 #include <fcntl.h>
 #include <assert.h>
 #include <time.h>
+
 #include "sys/ioctl.h"
+#include "cJSON.h"
+
+#define MAX_IMAGE_NO 30
+
+bool file_exists (char *filename) {
+	struct stat buffer;
+	return (stat(filename, &buffer) == 0);
+}
+
+
+char* load_file (char const* path)
+{
+	char* buffer = 0;
+	long length = 0;
+	FILE * f = fopen(path, "rb");
+
+	if (f)
+	{
+		fseek(f, 0, SEEK_END);
+		length = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		buffer = (char*)malloc((length+1)*sizeof(char));
+		if (buffer)
+		{
+			fread(buffer, sizeof(char), length, f);
+		}
+		fclose(f);
+	}
+	buffer[length] = '\0';
+
+	return buffer; 
+}
 
 static int is_charging = 0;
 void checkCharging(void) {
@@ -46,6 +81,34 @@ void SetBrightness(int value) {  // value = 0-10
 }
 
 int main(void) {
+	cJSON* request_json = NULL;
+	
+	cJSON* frame_count;
+	cJSON* frame_delay;
+	
+	int n_frame_count = 6;
+	int n_frame_delay = 50000;
+	
+	if (file_exists("/mnt/SDCARD/.tmp_update/chargingState.json") == 1) {
+		char *request_body = load_file("/mnt/SDCARD/.tmp_update/chargingState.json");
+		
+		if (request_body != NULL) {
+			request_json = cJSON_Parse(request_body);
+					
+			frame_count = cJSON_GetObjectItem(request_json, "frame_count");
+			frame_delay = cJSON_GetObjectItem(request_json, "frame_delay");
+			
+			n_frame_count = (int) cJSON_GetNumberValue(frame_count);
+			n_frame_delay = (int) cJSON_GetNumberValue(frame_delay);
+			
+			// Log json data
+			// char tmp[64];
+			// sprintf(tmp, "%d", n_frame_count);
+			// logMessage(tmp);
+		}
+	}
+	
+
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_ShowCursor(SDL_DISABLE);
 	   
@@ -53,50 +116,77 @@ int main(void) {
 	SDL_Surface* video = SDL_SetVideoMode(640,480, 32, SDL_HWSURFACE);
 	SDL_Surface* screen = SDL_CreateRGBSurface(SDL_HWSURFACE, 640,480, 32, 0,0,0,0);
 	
-	SDL_Surface* image0 = IMG_Load("chargingState0.png");
-	SDL_Surface* image1 = IMG_Load("chargingState1.png");
-	SDL_Surface* image2 = IMG_Load("chargingState2.png");
-	SDL_Surface* image3 = IMG_Load("chargingState3.png");
-	SDL_Surface* image4 = IMG_Load("chargingState4.png");
-	SDL_Surface* image5 = IMG_Load("chargingState5.png");
+	// SDL_Surface* image0 = IMG_Load("chargingState0.png");
+	// SDL_Surface* image1 = IMG_Load("chargingState1.png");
+	// SDL_Surface* image2 = IMG_Load("chargingState2.png");
+	// SDL_Surface* image3 = IMG_Load("chargingState3.png");
+	// SDL_Surface* image4 = IMG_Load("chargingState4.png");
+	// SDL_Surface* image5 = IMG_Load("chargingState5.png");
+	
+	SDL_Surface *images[MAX_IMAGE_NO];
+	
+	for (int i = 0; i < n_frame_count; i++) {
+		char temp[32];
+		sprintf(temp, "chargingState%d.png", i);
+		images[i] = IMG_Load(temp);
+	}
 	
 	SetBrightness(8);
 	int run = 1;
 
 	SDL_Event event;
 		
+	// for (int i = 1; i < 21; ++i){
+	// 	SDL_BlitSurface(image0, NULL, screen, NULL);
+	// 	SDL_BlitSurface(screen, NULL, video, NULL); 
+	// 	SDL_Flip(video);
+	// 	usleep(50000);
+	// 	
+	// 	SDL_BlitSurface(image1, NULL, screen, NULL);
+	// 	SDL_BlitSurface(screen, NULL, video, NULL); 
+	// 	SDL_Flip(video);
+	// 	usleep(50000);	
+	// 	
+	// 	SDL_BlitSurface(image2, NULL, screen, NULL);
+	// 	SDL_BlitSurface(screen, NULL, video, NULL); 
+	// 	SDL_Flip(video);
+	// 	usleep(50000);	
+	// 	
+	// 	SDL_BlitSurface(image3, NULL, screen, NULL);
+	// 	SDL_BlitSurface(screen, NULL, video, NULL); 
+	// 	SDL_Flip(video);
+	// 	usleep(50000);	
+	// 	
+	// 	SDL_BlitSurface(image4, NULL, screen, NULL);
+	// 	SDL_BlitSurface(screen, NULL, video, NULL); 
+	// 	SDL_Flip(video);
+	// 	usleep(50000);	
+	// 	
+	// 	SDL_BlitSurface(image5, NULL, screen, NULL);
+	// 	SDL_BlitSurface(screen, NULL, video, NULL); 
+	// 	SDL_Flip(video);
+	// 	usleep(50000);	
+	// 	
+	// 	checkCharging();
+	// 	if (is_charging == 0){
+	// 		break;
+	// 	}
+	// 	
+	// 	if(SDL_PollEvent(&event)){
+	// 		if ((event.type==SDL_KEYDOWN)&&(event.key.keysym.sym == 0)){
+	// 			run = 0;
+	// 			break;
+	// 		}
+	// 	}
+	// }
 	
-	for (int i = 1; i < 21; ++i){
-		
-		SDL_BlitSurface(image0, NULL, screen, NULL);
-		SDL_BlitSurface(screen, NULL, video, NULL); 
-		SDL_Flip(video);
-		usleep(50000);
-		
-		SDL_BlitSurface(image1, NULL, screen, NULL);
-		SDL_BlitSurface(screen, NULL, video, NULL); 
-		SDL_Flip(video);
-		usleep(50000);	
-		
-		SDL_BlitSurface(image2, NULL, screen, NULL);
-		SDL_BlitSurface(screen, NULL, video, NULL); 
-		SDL_Flip(video);
-		usleep(50000);	
-		
-		SDL_BlitSurface(image3, NULL, screen, NULL);
-		SDL_BlitSurface(screen, NULL, video, NULL); 
-		SDL_Flip(video);
-		usleep(50000);	
-		
-		SDL_BlitSurface(image4, NULL, screen, NULL);
-		SDL_BlitSurface(screen, NULL, video, NULL); 
-		SDL_Flip(video);
-		usleep(50000);	
-		
-		SDL_BlitSurface(image5, NULL, screen, NULL);
-		SDL_BlitSurface(screen, NULL, video, NULL); 
-		SDL_Flip(video);
-		usleep(50000);	
+	for (int i = 0; i < n_frame_count * 3; i++) {
+		for (int i = 0; i < n_frame_count; i++) {
+			SDL_BlitSurface(images[i], NULL, screen, NULL);
+			SDL_BlitSurface(screen, NULL, video, NULL); 
+			SDL_Flip(video);
+			usleep(n_frame_delay);	
+		}
 		
 		checkCharging();
 		if (is_charging == 0){
@@ -109,9 +199,7 @@ int main(void) {
 				break;
 			}
 		}
-	
 	}
-
 
 	
 	//usleep(5000000);	//5s
